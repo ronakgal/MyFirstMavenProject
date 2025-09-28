3d79099a5578e29f58f3d76e5741ac25a0e99c0b
ronakgal <ronakgal1996@gmail.com>
2025-09-27 22:55:25 -0700

    New Changes for Java File

---
+++ src/main/java/Firstpackage/Test2.java
@@ -0,0 +1,9 @@
+package Firstpackage;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Package");
+    }
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("mY fIRST pROGRAM");
+
+        //We need to create packages in order to maintain structued code
+        //Java is a blocking language
+        //Java is free form language
+    }
+}
